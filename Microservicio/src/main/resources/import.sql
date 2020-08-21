8e21706518924a990442fbcc38b9c0402ba2834a
Diego Roco <rockko.usach@gmail.com>
2020-08-21 19:37:01 -0400

    Primer Commit

---
+++ Microservicio/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('LG', 600, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Samsung', 500, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony', 400, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Adidas', 300, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Nike', 200, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Reebok', 100, NOW());
